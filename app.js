0b131398a7f35c215234070a51994905814c64f5
Hailey Stoudt <stoudth@flip2.engr.oregonstate.edu>
2024-02-27 16:16:04 -0800

    added sarah's views

--- app.js
+++ app.js
@@ -45,11 +45,12 @@ var db = require('./database/db-connector')
 // Handlebars - Node.js Starter App Step 3
 const { engine } = require('express-handlebars');
 var exphbs = require('express-handlebars');     // Import express-handlebars
+const { restart } = require('forever');
 app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
 app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters
 
 //Import Handlebars Helper for output date formatting - 'Handlebars date format issue' post on Stack Overflow: Answer from Simeon Babatunde and Tunaki
-var helpers = require('handlebars-helpers')();   
+//var helpers = require('handlebars-helpers')();   
 
 
 
@@ -79,7 +80,6 @@ app.get('/index', function(req, res)
 
 
 
-
 /*---------------------LOCATIONS--------------------------*/
 
 
@@ -92,19 +92,150 @@ app.get('/index', function(req, res)
 * Description of function: Handles Get requests and responses for the locations.hbs page - Renders the Locations page with Location data from the SQL Database
 * NOTE: The SQL query 'get_locations' is of our own creation
 * -----------END CITATION--------------
+*/            
+    
+    app.get('/locations', function(req, res)
+    {  
+        let query1 = "SELECT idLocation, locationName, streetAddress, city, state, zipcode FROM Locations ORDER BY idLocation;";               // Define our query
+
+        db.pool.query(query1, function(error, rows, fields){    // Execute the query
+
+            res.render('locations', {data: rows});                  // Render the index.hbs file, and also send the renderer
+        })                                                      // an object where 'data' is equal to the 'rows' we
+    });     
+
+    app.post('/add-location-ajax', function(req, res) 
+    {
+        // Capture the incoming data and parse it back to a JS object
+        let data = req.body;
+    
+            // Create the query and run it on the database
+            query1 = `INSERT INTO Locations (locationName, streetAddress, city, state, zipcode) VALUES ('${data.locationName}', '${data.streetAddress}', '${data.city}', '${data.state}', ${data.zipcode})`;
+            db.pool.query(query1, function(error, rows, fields){
+        
+                // Check to see if there was an error
+                if (error) {
+        
+                    // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                    console.log(error)
+                    res.sendStatus(400);
+                }
+                else
+                {
+                    // If there was no error, perform a SELECT * on bsg_people
+                    query2 = `SELECT * FROM Locations;`;
+                    db.pool.query(query2, function(error, rows, fields){
+        
+                        // If there was an error on the second query, send a 400
+                        if (error) {
+                            
+                            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                            console.log(error);
+                            res.sendStatus(400);
+                        }
+                        // If all went well, send the results of the query back.
+                        else
+                        {
+                            res.send(rows);
+                        }
+                    })
+                }
+            })
+    });
+    
+app.delete('/delete-location-ajax/', function(req,res,next){
+    let data = req.body;
+    let locationID = parseInt(data.idLocation)
+    console.log(locationID)
+    let deleteLocation = `DELETE FROM Locations WHERE idLocation = ?`; /* We'll let Cascade delete 
+    take care of the Sessions and Routes tables */
+        // Run the 1st query
+    db.pool.query(deleteLocation, [locationID], function(error, rows, fields){
+        if (error) {
+      
+        // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error);
+            res.sendStatus(400);
+        } else {       
+            res.sendStatus(204);  
+        }     
+    })
+});
+    
+app.put('/put-location-ajax', function(req,res,next){
+    let data = req.body;
+        
+    let location = parseInt(data.locationName);
+    let address = data.streetAddress;
+        
+    let queryUpdateAddress = `UPDATE Locations SET Locations.streetAddress = ? WHERE Locations.idLocation = ?`;
+    let selectAddress = `SELECT Locations.streetAddress FROM Locations WHERE Locations.idLocation = ?`;
+        
+    // Run the 1st query
+    db.pool.query(queryUpdateAddress, [address, location], function(error, rows, fields){
+        if (error) {
+        
+            // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+            console.log(error);
+            res.sendStatus(400);
+    
+        } else {
+            // Run the second query
+            db.pool.query(selectAddress, [location], function(error, rows, fields) {
+            
+                        if (error) {
+                            console.log(error);
+                            res.sendStatus(400);
+                        } else {
+                            res.send(rows);
+                        }
+                    })
+                }
+    })});
+
+/* -----------LOCATIONS POST/ADD_LOCATION ROUTE CITATION------------------
+* The below route handler for the Locations page was copied and adpated from Step 5 of the Node.js Start App provided in this course
+* Date: 2/22/2024
+* Copied and Adapted from: GitHub: osu-cs340-ecampus/nodejs-starter-app - Step 5
+* Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app
+* 
+* Description of function: Handles Post requests and responses for the locations.hbs page to add a new location to the database- Inserts a new class into the database and rerenders classes page to show update.
+* -----------END CITATION--------------
 */ 
 
-app.get('/locations', function(req, res)
+/*
+app.post('/add-location', function(req, res) 
     {
-        let get_locations = "SELECT idLocation, locationName, streetAddress, city, state, zipcode FROM Locations ORDER BY idLocation;";
 
-        db.pool.query(get_locations, function(error, rows, fields) {
+        let data = req.body;
 
-            res.render('locations', {data:rows});                  
-        });                                                      
-    });                                                     
+        //SQL query to add new location
+        let add_location = `INSERT INTO Locations(locationName, streetAddress, city, state, zipcode) Values ('${data.locationName}', '${data.streetAddress}', '${data.city}', '${data.state}', '${data.zipcode}');`;
 
+        //Handles if state abbreviation or zipcode are incorrect length
+        if (data.state.length !== 2 || data.zipcode.length !== 5) {
+            console.log('The input length for state or zipcode is incorrect. States should use the 2 letter abbreviation and zipcode should have 5 digits.');
+            res.sendStatus(400);
 
+        //Sends insert query to insert new location into database
+        } else {
+            db.pool.query(add_location, function(error, rows, fields){
+                if (error) {
+        
+                    console.log(error)
+                    res.sendStatus(400);
+    
+                } else {
+    
+                    res.redirect('locations');
+                }
+            })
+
+        }
+
+    }
+)
+*/
 
 /*---------------------CLASSES--------------------------*/
 
@@ -260,7 +391,7 @@ app.delete('/delete-class-ajax/', function(req, res, next)
 app.get('/sessions', function(req, res)
     {
         //Database queries - Gets session data, location data, and class data
-        let get_sessions = "SELECT Sessions.idSession AS idSession, Locations.locationName AS locationName, Classes.className AS className, classDate, Classes.sizeLimit As sizeLimit FROM Sessions INNER JOIN Locations ON Sessions.idLocation = Locations.idLocation LEFT JOIN Classes ON Sessions.idClass = Classes.idClass GROUP BY Sessions.idSession ORDER BY Sessions.idSession;"; 
+        let get_sessions = "SELECT Sessions.idSession AS idSession, Locations.locationName AS locationName, Classes.className AS className, DATE_FORMAT(classDate, '%Y-%m-%d') AS classDate, Classes.sizeLimit As sizeLimit FROM Sessions INNER JOIN Locations ON Sessions.idLocation = Locations.idLocation LEFT JOIN Classes ON Sessions.idClass = Classes.idClass GROUP BY Sessions.idSession ORDER BY Sessions.idSession;"; 
         let location_dropdown = "SELECT idLocation, locationName from Locations;";
         let class_dropdown = "SELECT idClass, className from Classes;";
 
@@ -283,6 +414,8 @@ app.get('/sessions', function(req, res)
             });
         });                                   
     });  
+
+
 
 
 /* -----------SESSIONS PUT/UPDATE-SESSION ROUTE CITATION------------------
@@ -340,7 +473,207 @@ app.put('/update-session', function(req, res) {
     });
 });
 
+/*---------------------ROUTES--------------------------*/
 
+/* -----------ROUTES GET ROUTE CITATION------------------
+* The below route handler for the Routes page was copied and adpated from Step 4 of the Node.js Start App provided in this course
+* Date: 2/21/2024
+* Copied and Adapted from: GitHub: osu-cs340-ecampus/nodejs-starter-app - Step 4
+* Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app
+* 
+* Description of function: Handles Get requests and responses for the routes.hbs page - Renders the Routes page with Routes data from the SQL Database
+*                     It also queries information from the database to create dropdown selections for Locations, RouteSetters, and RouteTypes in some forms on the page
+* NOTE: The SQL query 'get_routes', 'routesetter_drowdown', 'routetype_dropdown, and 'location_dropdown' are of our own creation
+* -----------END CITATION--------------
+*/ 
+
+
+app.get('/routes', function(req, res) {
+
+    let get_routes = `SELECT idRoute, routeName, DATE_FORMAT(dateSet, '%Y-%m-%d') AS dateSet, routeGrade, active, Locations.locationName AS locationName, Routes.idRouteSetter AS idRouteSetter, RouteSetters.firstName AS firstName, RouteSetters.lastName AS lastName, RouteTypes.routeType AS routeType FROM Routes JOIN Locations ON Routes.idLocation = Locations.idLocation JOIN RouteTypes ON Routes.idRouteType = RouteTypes.idRouteType LEFT JOIN RouteSetters ON Routes.idRouteSetter = RouteSetters.idRouteSetter ORDER BY idRoute;`; 
+    let location_dropdown = "SELECT idLocation, locationName from Locations;";
+    let routesetter_dropdown = "SELECT idRouteSetter, firstName, lastName FROM RouteSetters;";
+    let routetype_dropdown = "SELECT idRouteType, routeType FROM RouteTypes;"
+
+    //Gets Routes for display when page is rendered
+    db.pool.query(get_routes, function(error, rows, fields) {
+
+        let routes = rows
+
+        for (let i = 0, len = routes.length; i < len; i++) {
+            if (routes[i].firstName == null) {
+                routes[i].firstName = 'NULL'
+                routes[i].lastName = 'NULL'
+                routes[i].idRouteSetter = 'NULL'
+            };
+        };
+
+        db.pool.query(location_dropdown, function(error, rows, fields){
+
+            let locations = rows
+
+            db.pool.query(routesetter_dropdown, function(error, rows, fields){
+
+                let routesetters = rows;
+
+                db.pool.query(routetype_dropdown, function(error, rows, fields){
+
+                    let routetypes = rows;
+
+                    res.render('routes', {data:routes, locations:locations, routesetters:routesetters, routetypes:routetypes});
+
+                })
+            })
+        })
+    })  
+})
+
+
+/* -----------ROUTES POST/ADD_ROUTE ROUTE CITATION------------------
+* The below route handler for the Routes page was copied and adpated from Step 5 of the Node.js Start App provided in this course
+* Date: 2/22/2024
+* Copied and Adapted from: GitHub: osu-cs340-ecampus/nodejs-starter-app - Step 5
+* Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app
+* 
+* Description of function: Handles Post requests and responses for the routes.hbs page to add a new route to the database- Inserts a new route into the database and rerenders routes page to show update
+* NOTE: The SQL query 'add_route' is of our own creation
+*
+* -Hyphen Removal Citation
+* The below code that removes hypens from the dates was copied from the answer provided by James Hill (on Jun 1, 2011/edited Nov 26, 2012) on the 'Fastest way to remove hyphens from a string' page on StackOverflow
+* Date: 2/22/2024
+* Copied from answer provided by James Hill (on Jun 1, 2011/edited Nov 26, 2012) on the 'Fastest way to remove hyphens from a string' page on StackOverflow
+* Source URL: https://stackoverflow.com/questions/6204867/fastest-way-to-remove-hyphens-from-a-string
+* Authors: James Hill
+* -----------END CITATION--------------
+*/ 
+
+app.post('/add-route', function(req, res)
+    {
+        //Pull out and format data from request 
+        let data = req.body;
+        let dateSet = (data.dateSet).replace(/-/g,'');
+        let active = parseInt(data.active);
+        let idLocation = parseInt(data.locationName);
+        let idRouteSetter = data.idRouteSetter;
+        if (idRouteSetter != 'null') {
+            idRouteSetter = parseInt(idRouteSetter)
+        };
+        let idRouteType = parseInt(data.idRouteType);
+
+        // Database Insert Queries
+        let add_route = `INSERT INTO Routes(routeName, dateSet, routeGrade, active, idLocation, idRouteSetter, idRouteType) VALUES('${data.routeName}', ${dateSet}, '${data.routeGrade}', ${active}, ${idLocation}, ${idRouteSetter}, ${idRouteType});`;
+
+        //Add new route and handle errors
+        db.pool.query(add_route, function(error, rows, fields){
+            if (error) {
+    
+                console.log(error);
+                res.sendStatus(400);
+                    
+                    //If no error, reload classes page
+                } else {
+                    res.redirect('routes');
+            };
+        });
+    });
+
+
+app.put('/update-route', function(req, res) {
+    console.log(req.body)
+})
+
+
+
+/*
+   ROUTE TYPES ROUTES
+*/
+
+app.get('/routetypes', function(req, res)
+    {  
+        let getTypes = "SELECT * FROM RouteTypes;";               // Define our query
+
+        db.pool.query(getTypes, function(error, rows, fields){    // Execute the query
+
+            res.render('routetypes', {data: rows});                  
+        })                                                      
+    });    
+
+app.post('/add-routetype-ajax', function(req, res) 
+    {
+        // Capture the incoming data and parse it back to a JS object
+        let data = req.body;
+  
+        // Create the query and run it on the database
+        routeInsert = `INSERT INTO RouteTypes (routeType) VALUES ('${data.routeType}')`;
+        db.pool.query(routeInsert, function(error, rows, fields){
+    
+            // Check to see if there was an error
+            if (error) {
+    
+                // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                console.log(error)
+                res.sendStatus(400);
+            }
+            else
+            {
+                // If there was no error, perform a SELECT * on bsg_people
+                routeShow = `SELECT * FROM RouteTypes;`;
+                db.pool.query(routeShow, function(error, rows, fields){
+    
+                    // If there was an error on the second query, send a 400
+                    if (error) {
+                        
+                        // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                        console.log(error);
+                        res.sendStatus(400);
+                    }
+                    // If all went well, send the results of the query back.
+                    else
+                    {
+                        res.send(rows);
+                    }
+                })
+            }
+        })
+    });
+
+app.delete('/delete-routetype-ajax/', function(req,res,next){
+    let data = req.body;
+    let routeTypeID = parseInt(data.idRouteType);
+    let deleteRouteType = `DELETE FROM RouteTypes WHERE RouteTypes.idRouteType = ?`;
+    
+            // Run the 1st query
+            db.pool.query(deleteRouteType, [routeTypeID], function(error, rows, fields){
+                if (error) {
+    
+                // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                console.log(error);
+                res.sendStatus(400);
+                }
+    
+                else
+                {
+                    res.sendStatus(204);
+                }
+            })
+    });
+    
+/*
+   ROUTE SETTERS ROUTES
+*/
+
+app.get('/routesetters', function(req, res)
+    {  
+        let getSetters = "SELECT * FROM RouteSetters;";               // Define our query
+
+        db.pool.query(getSetters, function(error, rows, fields){    // Execute the query
+
+            res.render('routesetters', {data: rows});                  
+        })                                                      
+    });     
+    
+
+ 
 
 /* -----------LISTENER CITATION------------------
 * The below code is copied from from the Node.js Starter App referenced and linked to in this course. 
