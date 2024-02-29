c0c84c556bbc44320cb714fbec3f5533576daa92
skvanhoose <86205290+skvanhoose@users.noreply.github.com>
2024-02-28 19:55:14 -0600

    Update delete_locations.js
    adding citations

--- public/js/delete_locations.js
+++ public/js/delete_locations.js
@@ -1,3 +1,13 @@
+/* -----------DELETE LOCATION CITATION------------------
+* The below function is adapted from the Node.js Starter App provided in the Exploration - Web Application Technology 
+* Date: 2/22/2024 and 2/23/2024
+* Copied from: GitHub: osu-cs340-ecampus/nodejs-starter-app - Step 7 and bsg_HTML_UI file provided in the Exploration - Web Application Technology
+* Source URL: https://github.com/osu-cs340-ecampus/nodejs-starter-app and https://canvas.oregonstate.edu/courses/1946034/pages/exploration-web-application-technology?module_item_id=23809327
+* 
+* Description of function: Dynamically deletes a specified row from the table
+* -----------END CITATION--------------
+*/
+
 function deleteLocation(locationID) {
     console.log(locationID)
     let link = '/delete-location-ajax/';
